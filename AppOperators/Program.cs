dd6e7c4d4de04df24e24a21615952405d3f09900
GC <contact@gcteam.dev>
2022-12-03 12:53:30 -0800

    Add project files.

---
+++ AppOperators/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1, num2, num3;
+Console.WriteLine("Enter three numbers:");
+
+num1= Convert.ToInt32(Console.ReadLine());
+num2= Convert.ToInt32(Console.ReadLine());
+num3= Convert.ToInt32(Console.ReadLine());
+Console.ReadLine();
+Console.WriteLine("Multipications of three numbers entered: ");
+Console.WriteLine("******************");
+Console.WriteLine("Result: " + (num1*num2*num3));
